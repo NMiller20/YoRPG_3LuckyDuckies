f5d2cb0bf2479ef3dad8c42576bde7157146518d
Nora Miller <nmiller20@stuy.edu>
2021-11-22 22:20:06 -0500

    v1 done

--- v1/Monster.java
+++ v1/Monster.java
@@ -1,20 +1,28 @@
-public class Monster{
-  String nombre; // an instance variable for the protagonist's name
-  boolean isAlive;
 
-  public Monster() {
-  }
-  public Monster(String name) {
-    this();
-    nombre = name;
-  }
+  public class Monster extends Character {
 
-  public boolean isAlive(){ // instance method
-    return isAlive;
-  }
+    // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
+    private int _hitPts;
+    private int _strength;
+    private int _defense;
+    private double _attack;
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  public void attack(){
 
-  }
-  
-}
+    /**
+       default constructor
+       pre:  instance vars are declared
+       post: initializes instance vars.
+    **/
+    public Monster() {
+      _hitPts = 150;
+      _strength = 20 + (int)( Math.random() * 45 ); // [20,65)
+      _defense = 20;
+      _attack = 1;
+    }
+
+
+    // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
+
+
+  }//end class Monster
