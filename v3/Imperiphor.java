6bade065ba694a01983da3561838fc18e6b5d675
gloriaslee <glee20@stuy.edu>
2021-11-24 01:24:33 -0500

    v3 made

---
+++ v3/Imperiphor.java
@@ -0,0 +1,18 @@
+public class Imperiphor extends Monster {
+
+public static String about(){
+        String s = "ABOUT:";
+        s += "\nRoughly twice the size of a bear, the Imperiphor is a menace to any warrior who fights" ;
+        s += "\nfrom a medium or long distance, due to the difficulty of penetrating its scaly, metallic ";
+        s += "\nexterior. It is believed to be native to Staten Island, and insists, to anyone who will ";
+        s += "\nlisten, that its homeland is the most important part of New York City.";
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(about());
+    }//end main
+
+    // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
+
+  }//end class
---
+++ v3/Monster.java
@@ -0,0 +1,19 @@
+
+  public class Monster extends Character {
+
+    /**
+       default constructor
+       pre:  instance vars are declared
+       post: initializes instance vars.
+    **/
+    public Monster() {
+      _hitPts = 150;
+      _strength = 20 + (int)( Math.random() * 45 ); // [20,65)
+      _defense = 20;
+      _attackRating = 1;
+    }
+
+    // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
+
+
+  }//end class Monster
