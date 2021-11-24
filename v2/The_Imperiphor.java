df418235b5e18fd83a5d8da0fd5bbfd5459d7b05
Ziying Jian <zjian30@stuy.edu>
2021-11-23 22:42:19 -0500

    +about

--- v2/The_Imperiphor.java
+++ v2/The_Imperiphor.java
@@ -21,6 +21,18 @@
       _attack = 1;
     }
 
+public static String about(){
+        String s = "ABOUT:";
+        s += "\nRoughly twice the size of a bear, the Imperiphor is a menace to any warrior who fights" ;
+        s += "\nfrom a medium or long distance, due to the difficulty of penetrating its scaly, metallic ";
+        s += "\nexterior. It is believed to be native to Staten Island, and insists, to anyone who will ";
+        s += "\nlisten, that its homeland is the most important part of New York City.";
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(about());
+    }
 
     // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
 
--- v2/The_Salvisanguin.java
+++ v2/The_Salvisanguin.java
@@ -21,6 +21,17 @@
       _attack = 30;
     }
 
+public static String about(){
+        String s = "ABOUT:";
+        s += "\nResembling a vermillion penguin, the Salvisanguin heals itself after being wounded by concentrating";
+        s += "\nits life force to a special place inside of it, then circulating parts of it ";
+        s += "\nin the areas of its wounds. Because its favorite food is fish, its presence can generally ";
+        s += "\nbe smelled by those around it. ";
+       return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(about());
 
     // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
 
