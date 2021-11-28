8d4783b1a6ef78d2439e91fe387da3676e522080
gloriaslee <glee20@stuy.edu>
2021-11-28 17:26:39 -0500

    healing

--- v5/Salvisanguin.java
+++ v5/Salvisanguin.java
@@ -6,8 +6,7 @@ public class Salvisanguin extends Monster {
       s += "\nResembling a vermillion penguin, the Salvisanguin is an annoying little monster to deal with.";
       s += "\nBy concentrating its life force to a special place inside of it, then circulating parts of it, ";
       s += "\nit is able to generate a large number of total health points at the cost of a significantly decreased attack stat.";
-      s += "\nBecause its favorite food is fish, its presence can generally ";
-      s += "\nbe smelled by those around it. ";
+      s += "\nThis monster heals for a small amount each round.";
       s += "\n\tHEALTH:" + test._hitPts;
       s += "\n\tSTRENGTH:" + test._strength;
       s += "\n\tDEFENSE:" + test._defense;
@@ -20,6 +19,25 @@ public class Salvisanguin extends Monster {
     _defense = 20;
     _attackRating = 0.2;
   }
+
+  public int attack( Character opponent ) {
+    int roll = 0;
+    roll = (int)(Math.random()*3);
+    int damage = (int)( (_strength * _attackRating) - opponent.getDefense() );
+    if(roll == 2){
+      System.out.println("\t\tOUCH! You got unlucky! Ye Olde Monster unlocked a brief powerup.");
+      damage = damage * 3;
+    }
+
+    //System.out.println( "\t\t**DIAG** damage: " + damage );
+
+    if ( damage < 0 )
+      damage = 0;
+
+    opponent.lowerHP( damage );
+    heal(10);
+    return damage;
+  }//end attack
 
   public static void main(String[] args) {
       System.out.println(about());
