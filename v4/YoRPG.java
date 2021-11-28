0b9a3307b2187922b97f15430d49baf44e1a1349
gloriaslee <glee20@stuy.edu>
2021-11-28 12:56:57 -0500

    overwriting objects?

--- v4/YoRPG.java
+++ v4/YoRPG.java
@@ -122,6 +122,7 @@ public class YoRPG {
     while(true){ //true is so that the loop repeats infinitely unless it hits a break
        if(type == 1){
    			pat = new Bruiser(name);
+        pat = new Tank(name);
         break;
  		  }
  		  else if(type == 2){
