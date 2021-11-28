be19b601832964bd5465d924266b0b604a551393
gloriaslee <glee20@stuy.edu>
2021-11-28 13:05:43 -0500

    t

--- v4/Bruiser.java
+++ v4/Bruiser.java
@@ -45,11 +45,11 @@ public class Bruiser extends Protagonist{
         s += "\n\tBruisers are built for close combat!";
         s += "\n\tBruisers have higher total health points as well as higher strength and defense, but not as high as mages or tanks which are specialized!";
         s += "\n\tBruisers are well rounded fighters but don't have an especially high stat.";
-        /*s += "\n\tHEALTH:" + Bruiser._hitPts;
-        s += "\n\tSTRENGTH:" + Bruiser._strength;
-        s += "\n\tDEFENSE:" + Bruiser._defense;
-        s += "\n\tATTACK RATING:" + Bruiser._attackRating;
-       */return s;
+        s += "\n\tHEALTH:" + pat._hitPts;
+        s += "\n\tSTRENGTH:" + pat._strength;
+        s += "\n\tDEFENSE:" + pat._defense;
+        s += "\n\tATTACK RATING:" + pat._attackRating;
+       return s;
     }
     public static void main(String[] args) {
         System.out.println(about());
